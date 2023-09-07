81f3c496001382b8bf7b295f0e34f7331c1094f1
shahin hossain <shahinhossain3@hotmail.com>
2023-09-07 22:17:02 +0600

    cart items counter added & show modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,34 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar' //এটাকে বলে Relative Path & //default export function/component import
 import Card from './components/Card/Card';
 // import { Footer } from './components/Navbar/Navbar' //যদি একই folder থেকে ২টা component or function পাঠানো হয় তাহলে import করার পদ্ধতি হল
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
+
 function App() {
   const [guns, setGuns] = useState([]); //useState একটা Array Return করে
   const [cart, setCart] = useState([]);
+  const [modalIsOpen, setIsOpen] = useState(false); //Modal State
 
-
-
+  //Modal Open
+  function openModal() {
+    setIsOpen(true);
+  }
+  // Modal Close
+  function closeModal() {
+    setIsOpen(false);
+  }
   useEffect(() => {
     fetch('data.json')
       .then(res => res.json())
@@ -23,8 +45,9 @@ function App() {
 
   return (
     <>
-      <Navbar></Navbar>
-      <h2> added Cart: {cart.length}</h2>
+      <Navbar openModal={openModal} cart={cart}></Navbar>
+
+
       <div className='card-container'>
         {guns.map(gun => <Card
           key={gun.id}
@@ -32,6 +55,16 @@ function App() {
           handleAddToCart={handleAddToCart}
         ></Card>)}
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal} >Close</button>
+        <h2> added Cart: {cart.length}</h2>
+        {cart.map(gun => <h2>{gun.name}</h2>)}
+      </Modal> {/*Modal Component*/}
     </>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import './Navbar.css'
-const Navbar = () => {
+import './Navbar.css';
+import { BsCartCheck } from 'react-icons/bs';
+const Navbar = ({ openModal, cart }) => {
     return (
         <div className='navbar'>
             <h1>Kopa Samsu Store</h1>
+            <div className='cart-counter' onClick={openModal}>
+                <BsCartCheck className='icon' color='#ffffff' /> <span className='cart-counter'>{cart.length}</span>{/*Modal Button*/}
+            </div>
         </div>
     );
 };
